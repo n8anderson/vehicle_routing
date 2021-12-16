17b613a7a53bb028d6878235505586f9b3be3b9a
n8anderson <bsuCompSci2021!>
2021-12-16 09:41:44 -0500

    Finalized styling, last two problems and assets

--- get_routes.py
+++ get_routes.py
@@ -30,7 +30,6 @@ def populate_map():
 
     print(list_of_locs)
 
-
     list_of_vals = []
     while len(list_of_vals) < 10:
         list_of_vals.append(r.randint(10, 50))
@@ -100,7 +99,7 @@ def run_vehicle(window: gui.StandardWindow):
 def calc_subs_nodes(vehicle: Vehicle, v_nodes: List, depot: Node, window: gui.StandardWindow, stops):
     display_route = print_route_map(stops)
     window.route_display.setText(display_route)
-    #print(display_route)
+    # print(display_route)
     if vehicle.vehicle_ID == 1:
         window.print_text += "\n--------------------------------VEHICLE 2--------------------------------"
     window.print_text += ("\n" + vehicle.__toString__())
@@ -110,7 +109,6 @@ def calc_subs_nodes(vehicle: Vehicle, v_nodes: List, depot: Node, window: gui.St
     else:
         stops[vehicle.nodes_visited[0].location[0]][vehicle.nodes_visited[0].location[1]] = 'V1'
     final_node = None
-    count = 0
     while len(vehicle.nodes_visited) < len(v_nodes):
         display_route = print_route_map(stops)
         window.route_display.setText(display_route)
@@ -121,18 +119,12 @@ def calc_subs_nodes(vehicle: Vehicle, v_nodes: List, depot: Node, window: gui.St
         saved_node = None
         for node in v_nodes:
             if node not in vehicle.nodes_visited:
-                print("COUNT #", count)
-
                 delta_y = v_pos[1] - node.location[1]
                 delta_x = v_pos[0] - node.location[0]
                 distance = math.sqrt((delta_x ** 2) + (delta_y ** 2))
-                print("Current Pos", v_pos, "Node Pos", node.location, "Distance", distance)
                 if distance < shortest_distance:
                     shortest_distance = distance
                     saved_node = node
-                print("SHORTEST DISt", shortest_distance)
-        count += 1
-        print()
         window.handleStart()
         vehicle.move_to_node(saved_node)
         if vehicle.vehicle_ID == 0:
@@ -153,12 +145,185 @@ def calc_subs_nodes(vehicle: Vehicle, v_nodes: List, depot: Node, window: gui.St
     vehicle.move_to_node(depot)
     window.print_text += ("\nFinal Total: " + vehicle.__toString__())
 
+    return vehicle
+
+
+def limited_stops(window: gui.LimitedWindow):
+    stops, locs = populate_map()
+
+    print_route_map(stops)
+    depot = stops[4][4]
+    depot_loc = depot.location
+
+    vehicle1 = Vehicle(depot_loc, 0)
+    vehicle2 = Vehicle(depot_loc, 1)
+
+    vehicle1_route = []
+    vehicle2_route = []
+
+    for loc in locs:
+        if stops[loc[0]][loc[1]].vehicle_id == 0:
+            vehicle1_route.append(stops[loc[0]][loc[1]])
+        else:
+            vehicle2_route.append(stops[loc[0]][loc[1]])
+
+    # vehicle2.move_to_node(calc_first_node(vehicle2, vehicle2_route))
+
+    vehicle1 = calc_limited_nodes(vehicle1, vehicle1_route, depot, window, stops)
+    vehicle2 = calc_limited_nodes(vehicle2, vehicle2_route, depot, window, stops)
+
+    totals = f"Vehicle 1 Info:\tDistance: {vehicle1.distance_traveled}\tValue: {vehicle1.total_revenue}\nVehicle 2 Info:\tDistance: {vehicle2.distance_traveled}\tValue: {vehicle2.total_revenue}\n"
+    window.totals_display.setText(totals)
+
+
+def calc_limited_nodes(vehicle: Vehicle, v_nodes: List, depot: Node, window: gui.StandardWindow, stops):
+    display_route = print_route_map(stops)
+    window.route_display.setText(display_route)
+    # print(display_route)
+    if vehicle.vehicle_ID == 1:
+        window.print_text += "\n--------------------------------VEHICLE 2--------------------------------"
+    window.print_text += ("\n" + vehicle.__toString__())
+    vehicle.move_to_node(calc_highest_val(v_nodes))
+    if vehicle.vehicle_ID == 0:
+        stops[vehicle.nodes_visited[0].location[0]][vehicle.nodes_visited[0].location[1]] = 'V0'
+    else:
+        stops[vehicle.nodes_visited[0].location[0]][vehicle.nodes_visited[0].location[1]] = 'V1'
+    final_node = None
+
+    while len(vehicle.nodes_visited) < 3:
+        display_route = print_route_map(stops)
+        window.route_display.setText(display_route)
+        window.print_text += ("\n" + vehicle.__toString__())
+        window.display.setText(window.print_text)
+        saved_node = None
+        highest_val = 0
+        for node in v_nodes:
+            if node not in vehicle.nodes_visited:
+                if node.value > highest_val:
+                    highest_val = node.value
+                    saved_node = node
+        window.handleStart()
+        vehicle.move_to_node(saved_node)
+        if vehicle.vehicle_ID == 0:
+            stops[saved_node.location[0]][saved_node.location[1]] = 'V0'
+        else:
+            stops[saved_node.location[0]][saved_node.location[1]] = 'V1'
+        final_node = saved_node
+
+    if vehicle.vehicle_ID == 0:
+        stops[final_node.location[0]][final_node.location[1]] = 'V0'
+    else:
+        stops[final_node.location[0]][final_node.location[1]] = 'V1'
+    display_route = print_route_map(stops)
+    window.route_display.setText(display_route)
+    window.print_text += ("\n" + vehicle.__toString__())
+    window.display.setText(window.print_text)
+    vehicle.move_to_node(depot)
+    window.print_text += ("\nFinal Total: " + vehicle.__toString__())
 
     return vehicle
 
 
-def check_press():
-    return True
+def short_route(window: gui.ShortestWindow):
+    stops, locs = populate_map()
+
+    all_stops = []
+    for i in locs:
+        all_stops.append(stops[i[0]][i[1]])
+
+    print_route_map(stops)
+    depot = stops[4][4]
+    depot_loc = depot.location
+
+    vehicle1 = Vehicle(depot_loc, 0)
+    vehicle2 = Vehicle(depot_loc, 1)
+
+    # vehicle2.move_to_node(calc_first_node(vehicle2, vehicle2_route))
+
+    try:
+        vehicle1, vehicle2 = run_shortest_path([vehicle1, vehicle2], all_stops, depot, window, stops)
+    except Exception as e:
+        print(Exception, e)
+
+    totals = f"Vehicle 1 Info:\tDistance: {vehicle1.distance_traveled}\tValue: {vehicle1.total_revenue}\nVehicle 2 Info:\tDistance: {vehicle2.distance_traveled}\tValue: {vehicle2.total_revenue}\n"
+    window.totals_display.setText(totals)
+
+
+def run_shortest_path(vehicle: List[Vehicle], v_nodes: List, depot: Node, window: gui.ShortestWindow, stops):
+    visited_nodes = []
+    display_route = print_route_map(stops)
+    window.route_display.setText(display_route)
+    window.print_text += ("\n" + vehicle[0].__toString__())
+    window.print_text += ("\n" + vehicle[1].__toString__())
+    temp_node = calc_short_dist(vehicle[0], v_nodes, visited_nodes)
+    visited_nodes.append(temp_node)
+    vehicle[0].move_to_node(temp_node)
+    temp_node = calc_short_dist(vehicle[1], v_nodes, visited_nodes)
+    visited_nodes.append(temp_node)
+    vehicle[1].move_to_node(temp_node)
+    stops[vehicle[0].nodes_visited[0].location[0]][vehicle[0].nodes_visited[0].location[1]] = 'V0'
+    stops[vehicle[1].nodes_visited[0].location[0]][vehicle[1].nodes_visited[0].location[1]] = 'V1'
+    final_node = None
+    while len(visited_nodes) < len(v_nodes):
+        print(1)
+        display_route = print_route_map(stops)
+        window.route_display.setText(display_route)
+        window.print_text += ("\n" + vehicle[0].__toString__())
+        window.print_text += ("\n" + vehicle[1].__toString__())
+        window.display.setText(window.print_text)
+        temp_node = calc_short_dist(vehicle[0], v_nodes, visited_nodes)
+        visited_nodes.append(temp_node)
+        vehicle[0].move_to_node(temp_node)
+        temp_node = calc_short_dist(vehicle[1], v_nodes, visited_nodes)
+        visited_nodes.append(temp_node)
+        vehicle[1].move_to_node(temp_node)
+
+        stops[vehicle[0].current_pos[0]][vehicle[0].current_pos[1]] = 'V0'
+        stops[vehicle[1].current_pos[0]][vehicle[1].current_pos[1]] = 'V1'
+        # final_node = temp_node
+        window.handleStart()
+
+    stops[vehicle[0].current_pos[0]][vehicle[0].current_pos[1]] = 'V0'
+    stops[vehicle[1].current_pos[0]][vehicle[1].current_pos[1]] = 'V1'
+    display_route = print_route_map(stops)
+    window.route_display.setText(display_route)
+    window.print_text += ("\n" + vehicle[0].__toString__())
+    window.print_text += ("\n" + vehicle[1].__toString__())
+    window.display.setText(window.print_text)
+    vehicle[0].move_to_node(depot)
+    vehicle[1].move_to_node(depot)
+    window.print_text += ("\nFinal Total: " + vehicle[0].__toString__())
+    window.print_text += ("\nFinal Total: " + vehicle[1].__toString__())
+
+    return vehicle
+
+
+def calc_short_dist(vehicle: Vehicle, v_nodes: List, visited_nodes: List[Node]):
+    v_pos = vehicle.current_pos
+    shortest_distance = sys.maxsize
+    saved_node = None
+    for node in v_nodes:
+        if node not in visited_nodes:
+            delta_y = v_pos[1] - node.location[1]
+            delta_x = v_pos[0] - node.location[0]
+            distance = math.sqrt((delta_x ** 2) + (delta_y ** 2))
+            if distance < shortest_distance:
+                shortest_distance = distance
+                saved_node = node
+
+    return saved_node
+
+
+def calc_highest_val(v_nodes: List):
+    saved_node = None
+    highest_val = 0
+
+    for node in v_nodes:
+        if node.value > highest_val:
+            highest_val = node.value
+            saved_node = node
+
+    return saved_node
 
 
 def calc_first_node(vehicle: Vehicle, v_nodes: List):
--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import time
 
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QDialog, QVBoxLayout
 import get_routes
 
@@ -11,16 +11,43 @@ import get_routes
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setStyleSheet("background-color: gray;")
 
         self.standard_vrp_button = QPushButton(self)
+        self.limited_vrp_button = QPushButton(self)
+        self.shortest_vrp_button = QPushButton(self)
         self.standard_label = QLabel(self)
         self.standard_label.setText("Touching every node, with shortest distance and back to depot.")
+
+        self.limited_label = QLabel(self)
+        self.limited_label.setText("Highest Value for Max 3 Stops.")
+
+        self.shortest_label = QLabel(self)
+        self.shortest_label.setText("Shortest Distance with no defined route.")
+
+        self.pixmap = QPixmap('start.png')
+
+        self.pix_label = QLabel(self)
+        self.pix_label.setPixmap(self.pixmap)
+
+        self.pix_label.resize(self.pixmap.width(), self.pixmap.height())
+
+        self.pix_label.move(650, 50)
+
+        self.pixmap2 = QPixmap('start2.png')
+
+        self.pix_label2 = QLabel(self)
+        self.pix_label2.setPixmap(self.pixmap2)
+
+        self.pix_label2.resize(self.pixmap2.width(), self.pixmap2.height())
+
+        self.pix_label2.move(50, 450)
 
         self.setup()
 
     def setup(self):
         self.setWindowTitle("Vehicle Routing Problem Solutions")
-        self.setGeometry(50, 50, 960, 960)
+        self.setGeometry(768, 462, 1200, 900)
 
         self.standard_vrp_button.resize(200, 40)
         self.standard_vrp_button.move(50, 100)
@@ -30,20 +57,188 @@ class MainWindow(QMainWindow):
         self.standard_label.resize(600, 40)
         self.standard_label.move(50, 150)
 
+        self.limited_vrp_button.resize(200, 40)
+        self.limited_vrp_button.move(50, 200)
+        self.limited_vrp_button.setText("Limited Solution")
+        self.limited_vrp_button.clicked.connect(self.to_limited)
+
+        self.limited_label.resize(600, 40)
+        self.limited_label.move(50, 250)
+
+        self.shortest_vrp_button.resize(200, 40)
+        self.shortest_vrp_button.move(50, 300)
+        self.shortest_vrp_button.setText("Shortest Solution")
+        self.shortest_vrp_button.clicked.connect(self.to_shortest)
+
+        self.shortest_label.resize(600, 40)
+        self.shortest_label.move(50, 350)
+
         self.show()
+
+    def to_shortest(self):
+        self.cams = ShortestWindow()
+        self.cams.show()
+        self.close()
+
+    def to_limited(self):
+        self.cams = LimitedWindow()
+        self.cams.show()
+        self.close()
 
     def to_standard(self):
         self.cams = StandardWindow()
         self.cams.show()
         self.close()
 
+class LimitedWindow(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: gray;")
+        self.title = "Limited Stop VRP"
+        self.next_button = QPushButton(self)
+        self.back_button = QPushButton(self)
+        self.start_button = QPushButton(self)
+        self.pressed = False
+        self.route_display = QLabel()
+        self.route_display.setFont(QFont('Arial', 18))
+        self.totals_display = QLabel()
+        self.totals_display.setFont(QFont('Arial', 16))
+        self.display = QLabel()
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.route_display, stretch=True)
+        self.vbox.addWidget(self.display, stretch=True)
+        self.vbox.addWidget(self.totals_display, stretch=True)
+        self.vbox.addWidget(self.next_button, stretch=True)
+        self.vbox.addWidget(self.start_button, stretch=True)
+        self.vbox.addWidget(self.back_button, stretch=True)
+        self.setLayout(self.vbox)
+        self.print_text = f""
+
+        self.setup()
+
+    def setup(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(768, 462, 1200, 900)
+
+        self.back_button.resize(150, 40)
+        self.back_button.move(50, 860)
+        self.back_button.setText("Back to Main")
+        self.back_button.clicked.connect(self.back_to_main)
+
+        self.start_button.resize(150, 40)
+        self.start_button.move(50, 760)
+        self.start_button.setText("Start")
+        self.start_button.clicked.connect(self.start_vrp)
+
+        self.next_button.resize(150, 40)
+        self.next_button.move(250, 760)
+        self.next_button.setText("Next Step")
+        self.next_button.clicked.connect(self.handleStop)
+        self.show()
+
+    def start_vrp(self):
+        get_routes.limited_stops(self)
+
+    def handleStart(self):
+        try:
+            self.start_button.setDisabled(True)
+            self._running = True
+            while self._running:
+                QtGui.QGuiApplication.processEvents()
+                time.sleep(0.05)
+            self.start_button.setDisabled(False)
+        except Exception as e:
+            print(Exception, e)
+
+    def handleStop(self):
+        self._running = False
+
+    def back_to_main(self):
+        try:
+            self.cams = MainWindow()
+            self.cams.show()
+            self.close()
+        except Exception as e:
+            print(Exception, e)
+
+class ShortestWindow(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet("background-color: gray;")
+        self.title = "Shortest VRP"
+        self.next_button = QPushButton(self)
+        self.back_button = QPushButton(self)
+        self.start_button = QPushButton(self)
+        self.pressed = False
+        self.route_display = QLabel()
+        self.route_display.setFont(QFont('Arial', 18))
+        self.totals_display = QLabel()
+        self.totals_display.setFont(QFont('Arial', 16))
+        self.display = QLabel()
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.route_display, stretch=True)
+        self.vbox.addWidget(self.display, stretch=True)
+        self.vbox.addWidget(self.totals_display, stretch=True)
+        self.vbox.addWidget(self.next_button, stretch=True)
+        self.vbox.addWidget(self.start_button, stretch=True)
+        self.vbox.addWidget(self.back_button, stretch=True)
+        self.setLayout(self.vbox)
+        self.print_text = f""
+
+        self.setup()
+
+    def setup(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(768, 462, 1200, 900)
+
+        self.back_button.resize(150, 40)
+        self.back_button.move(50, 860)
+        self.back_button.setText("Back to Main")
+        self.back_button.clicked.connect(self.back_to_main)
+
+        self.start_button.resize(150, 40)
+        self.start_button.move(50, 760)
+        self.start_button.setText("Start")
+        self.start_button.clicked.connect(self.start_vrp)
+
+        self.next_button.resize(150, 40)
+        self.next_button.move(250, 760)
+        self.next_button.setText("Next Step")
+        self.next_button.clicked.connect(self.handleStop)
+        self.show()
+
+    def start_vrp(self):
+        get_routes.short_route(self)
+
+    def handleStart(self):
+        try:
+            self.start_button.setDisabled(True)
+            self._running = True
+            while self._running:
+                QtGui.QGuiApplication.processEvents()
+                time.sleep(0.05)
+            self.start_button.setDisabled(False)
+        except Exception as e:
+            print(Exception, e)
+
+    def handleStop(self):
+        self._running = False
+
+    def back_to_main(self):
+        try:
+            self.cams = MainWindow()
+            self.cams.show()
+            self.close()
+        except Exception as e:
+            print(Exception, e)
 
 class StandardWindow(QDialog):
 
     def __init__(self):
         super().__init__()
-        self.width = 960
-        self.height = 960
+        self.setStyleSheet("background-color: gray;")
         self.title = "Standard VRP"
         self.next_button = QPushButton(self)
         self.back_button = QPushButton(self)
@@ -68,7 +263,7 @@ class StandardWindow(QDialog):
 
     def setup(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(50, 50, self.width, self.height)
+        self.setGeometry(768, 462, 1200, 900)
 
         self.back_button.resize(150, 40)
         self.back_button.move(50, 860)
